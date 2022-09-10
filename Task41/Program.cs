d61236fcf7e613ff1d2385308d06d68a245ae423
Kate <vvellspring@gmail.com>
2022-09-10 20:28:30 +0300

    Задача 41 сколько чисел больше 0 ввел пользователь

---
+++ Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.Clear();
+Console.Write("Введите числа через запятую: ");
+string[] st = Console.ReadLine().Split(',',StringSplitOptions.RemoveEmptyEntries);
+int count = 0;
+for (int i = 0; i < st.Length; i++)
+{
+    if (int.Parse(st[i]) > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество чисел больше 0 = {count}");
